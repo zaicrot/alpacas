3ada22e8343fd79042ff60223e49f376bcd44020
Victor Raul <v1c70rotakmer@gmail.com>
2024-07-18 22:51:50 -0500

    update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,19 @@
 import Main from './pages/Main'
 import './App.css'
-
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
+import Manage from './pages/Manage'
+import Products from './pages/Products'
 function App() {
     return (
         <div>
-            <Main />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/products" element={<Products />} />
+                    {/* <Route path="/products/:id" element={<Product />} /> */}
+                    <Route path="/manage" element={<Manage />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
